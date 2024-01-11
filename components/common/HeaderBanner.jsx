9d5e37b130ab65259ae3a18d0d87b5997d17d6e1
unknown <sonukukna27@gmail.com>
2024-01-11 10:45:47 +0530

    some changes

--- components/common/HeaderBanner.jsx
+++ components/common/HeaderBanner.jsx
@@ -2,21 +2,23 @@ import { Arrowicon } from "./Icon";
 
 const HeaderBanner = ({ heading }) => {
   return (
-    <div className="bg-[url('/img/Hero-bg.png')] bg-no-repeat bg-center bg-cover pb-5 sm:pb-[38px] md:pb-[48px] pt-5 sm:pt-10 md:pt-[50px] px-5 sm:px-10 md:px-[50px]">
-      <p className="text-white font-Roboto text-2xl sm:text-[33px] font-medium ">
-        {heading}
-      </p>
-
-      <div className="h-full pt-3 sm:pt-[22px] flex items-center justify-start gap-[10px]">
-        <p className="text-[hsla(0,0%,100%,1)] font-Roboto text-[16px] font-normal">
-          Home
-        </p>
-        <div className="pt-[5px]">
-          <Arrowicon />
-        </div>
-        <p className="text-white font-Roboto text-[16px] font-medium">
+    <div className="max-w-[1920px] w-full  bg-[url('/img/Hero-bg.png')] mx-auto">
+      <div className=" max-w-[1152px] mx-auto  bg-no-repeat bg-center bg-cover pb-5 sm:pb-[38px] md:pb-[48px] pt-5 sm:pt-10 md:pt-[50px] px-5 sm:px-10 md:px-[50px]">
+        <p className="text-white font-Roboto text-2xl sm:text-[33px] font-medium ">
           {heading}
         </p>
+
+        <div className="h-full pt-3 sm:pt-[22px] flex items-center justify-start gap-[10px]">
+          <p className="text-[hsla(0,0%,100%,1)] font-Roboto text-[16px] font-normal">
+            Home
+          </p>
+          <div className="pt-[5px]">
+            <Arrowicon />
+          </div>
+          <p className="text-white font-Roboto text-[16px] font-medium">
+            {heading}
+          </p>
+        </div>
       </div>
     </div>
   );
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,11 @@
 import HomePage from "@/components/HomePage";
 import Layout from "@/components/common/Layout";
+import Gallery from "./gallery";
 
 export default function Home() {
   return (
     <>
-      <Layout>
-        <HomePage />
-      </Layout>
+      <Gallery />
     </>
   );
 }
